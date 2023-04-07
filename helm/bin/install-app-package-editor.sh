d8b87b547216860e7ab23de6ff72a7cf129b8b31
Jannes Lathouwers <jannes.lathouwers@spaceapplications.com>
2023-04-07 15:05:14 +0000

    Feature: ci cd implementation

--- helm/bin/install-app-package-editor.sh
+++ helm/bin/install-app-package-editor.sh
@@ -4,6 +4,6 @@ source ./bash-styles.sh
 ./create-namespace.sh ap-editor
 
 echo -e "${YELLOW}Installing EO Application Package Editor...${NO_COLOR}"
-helm upgrade --install ap-editor ../charts/eo-application-package-editor/ \
+helm upgrade --install ap-editor ../eo-application-package-editor/ \
   --namespace ap-editor \
-  --values ../values/eo-application-package-editor/values.yaml
+  --values ../values/develop/values.yaml
